fb1912426670a6c21433a2b5bf35adceaef71bc1
joao-victor-campos <mmoraes.victor@gmail.com>
2022-01-19 21:22:07 -0300

    refactoring create_table function

--- create_table.py
+++ create_table.py
@@ -1,5 +1,5 @@
-import pandas as pd
-from sqlalchemy import create_engine
-df = pd.read_csv('/home/joao_victor/repos/brazilian_election_data_analysis/election_data/perfil_eleitorado_1994.csv', sep = ';' , header = 0, encoding = 'latin1')
-engine = create_engine('postgresql://postgres:mysecretpassword@localhost:5432/postgres')
-df.to_sql('perfil_eleitorado_1994', engine)
+from zip_handler.functions import create_table
+from zip_handler.functions import data_path, create_list
+
+file_list = create_list(data_path)
+create_table(file_list)
--- zip_handler/functions.py
+++ zip_handler/functions.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 from zipfile import ZipFile
+import pandas as pd
+from sqlalchemy import create_engine
+
 
 data_path = Path.home()
 data_path = data_path.joinpath('repos/brazilian_election_data_analysis/election_data/')
@@ -14,3 +17,18 @@ def extractor (file_name):
 def delete (file_name):
     for i in file_name:
         os.remove(data_path.joinpath(i))
+
+def create_list (data_path):
+    file_list = []
+    for (dirpath, dirnames, filenames) in os.walk(data_path):
+        file_list.extend(filenames)
+        break
+    file_list.sort()
+    return file_list
+
+def create_table (file_name):
+    
+    for i in file_name:
+        df = pd.read_csv(data_path.joinpath(i), sep = ';' , header = 0, encoding = 'latin1')
+        engine = create_engine('postgresql://postgres:mysecretpassword@localhost:5432/postgres')
+        df.to_sql(i, engine)
